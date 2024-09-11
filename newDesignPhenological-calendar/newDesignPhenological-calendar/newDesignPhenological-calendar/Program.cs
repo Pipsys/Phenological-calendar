7f42849e0faf96c3f60ccbba2b198071020e404e
Igor Davidov <igordavidovsteam@gmail.com>
2024-09-11 14:01:38 +0300

    Добавил UseWebRoot

--- newDesignPhenological-calendar/newDesignPhenological-calendar/newDesignPhenological-calendar/Program.cs
+++ newDesignPhenological-calendar/newDesignPhenological-calendar/newDesignPhenological-calendar/Program.cs
@@ -19,7 +19,8 @@ namespace newDesignPhenological_calendar
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<DataService>();
-            
+            builder.WebHost.UseWebRoot("wwwroot");
+            builder.WebHost.UseStaticWebAssets();
             
             var app = builder.Build();
 
@@ -30,7 +31,6 @@ namespace newDesignPhenological_calendar
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
             
 
             app.UseHttpsRedirection();
